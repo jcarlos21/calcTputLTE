164e67ef857330504fde2cb2ac416bc5b7d53255
jcarlos21 <j.carlos2020@live.com>
2022-11-16 20:22:25 -0300

    Commit inicial do projeto calcTputLTE

---
+++ calcTputLTE.py
@@ -0,0 +1,17 @@
+from dados import *
+
+class calcTputLTE:
+    def __init__(self, BW, PRB, SP, CP, MIMO, modulation, MSC, TBS, valueTBS):
+        self.BW = BW
+        self.PRB = PRB
+        self.SP = SP
+        self.CP = CP
+        self.MIMO = MIMO
+        self.modulation = modulation
+        self.MSC = MSC
+        self.
+
+        pass
+
+    def calcTputLTE(self):
+        return self.value
