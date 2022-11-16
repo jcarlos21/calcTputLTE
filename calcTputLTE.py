ef11c304f11ddab204f27db0ae1cdabc846744bc
jcarlos21 <j.carlos2020@live.com>
2022-11-16 20:40:19 -0300

    Alterando classe calcTputLTE

--- calcTputLTE.py
+++ calcTputLTE.py
@@ -1,17 +1,19 @@
 from dados import *
 
 class calcTputLTE:
-    def __init__(self, BW, PRB, SP, CP, MIMO, modulation, MSC, TBS, valueTBS):
-        self.BW = BW
-        self.PRB = PRB
-        self.SP = SP
-        self.CP = CP
-        self.MIMO = MIMO
+    def __init__(self, bw, prb, sp, cp, mimo, modulation, msc, tbs, valueTBS, carrieAggregation):
+        self.bw = bw
+        self.prb = prb
+        self.sp = sp
+        self.cp = cp
+        self.mimo = mimo
         self.modulation = modulation
-        self.MSC = MSC
-        self.
-
-        pass
+        self.msc = msc
+        self.tbs = tbs
+        self.valueTBS = valueTBS
+        self.carrieAggregation = carrieAggregation
 
     def calcTputLTE(self):
-        return self.value
+        return (self.valueTBS * 1000 * self.mimo * self.carrieAggregation).__round(2)
+    
+    
