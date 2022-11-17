b0bfbda2e977644edf6b2e50da8d80955300376e
jcarlos21 <j.carlos2020@live.com>
2022-11-17 00:30:12 -0300

    Configurando interface

---
+++ iTBS.py
@@ -0,0 +1,21 @@
+rb = [6, 15, 25, 50, 75, 100]
+tbsIndex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 10, 11, 12, 13, 14, 15, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26]
+
+nome_arquivo = 'valueTBS.txt'
+arquivo = open(nome_arquivo)
+linhas = arquivo.readlines()
+
+v_TBS = list()
+for i in range(len(linhas)):    
+    v_TBS.append(linhas[i].replace('\n', '').split(','))
+
+vTBS = list()
+for i in range(len(v_TBS)):
+    vTBS.append(list(map(int, v_TBS[i])))
+
+def valueTBS(valorTBS, valorRB):
+    id1 = tbsIndex.index(valorTBS)
+    id2 = rb.index(valorRB)
+    print(id1)
+    print(id2)
+    return vTBS[id1][id2]  # Valor do TBS com base em RB e índice TBS
--- main.py
+++ main.py
@@ -1,2 +1,23 @@
-import tkinter
-tkinter._test()
+from tkinter import *
+from tkinter import Tk
+
+class Application:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("LTE Throughput Calculator")
+        self.root.configure(background="blue")
+        self.root.geometry("550x350")
+        self.root.iconbitmap("img/venda1.ico")
+        self.root.resizable(False, False)
+        
+        self.backGroundImage = PhotoImage(file="img/background.png")
+        self.label = Label(self.root, image=self.backGroundImage)
+        self.label.place(x=0, y=0)
+        self.label.image = self.backGroundImage
+
+
+root = Tk()
+Application(root)
+# root.mainloop()
+if __name__ == "__main__":
+    root.mainloop()
